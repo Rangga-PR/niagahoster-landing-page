5794e68cb5bfcf3885a1c60846d1192a53d2b24e
Rangga Putra Raharja <rangga-pr@pop-os.localdomain>
2020-02-27 21:25:01 +0700

    add custom button component

---
+++ resources/js/components/custombuton/CustomButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="button" :class="[primary ? 'primary' : 'secondary']">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "CustomButton",
+    props: {
+        text: String,
+        primary: Boolean
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+    padding: 9px 15px;
+    border-radius: 100px;
+    font-weight: bolder;
+}
+
+.primary {
+    color: $white;
+    background-color: $light-blue;
+    border: 2px solid $light-blue;
+}
+
+.secondary {
+    color: $black;
+    background-color: $white;
+    border: 2px solid $black;
+}
+</style>
